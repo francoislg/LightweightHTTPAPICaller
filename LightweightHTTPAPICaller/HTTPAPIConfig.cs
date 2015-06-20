9c99d79bad8e6f9cfabe35f568754bc7e9ca4550
Francois Lachance-Guillemette <francois_lg@hotmail.com>
2015-06-20 15:46:28 -0400

    Client for API caller with simple setup

---
+++ LightweightHTTPAPICaller/HTTPAPIConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightweightHTTPAPICaller {
+    public class HTTPAPIConfig {
+        public String httpAddress { get; private set; }
+        public String apiKey { get; private set; }
+        public ResponseType responseType { get; set; }
+
+        public HTTPAPIConfig(String httpAddress) {
+            this.httpAddress = httpAddress;
+            this.responseType = ResponseType.JSON;
+        }
+
+        public string getResponseType() {
+            switch (responseType) {
+                case ResponseType.JSON:
+                    return "json";
+                case ResponseType.XML:
+                    return "xml";
+                default:
+                    return "json";
+            }
+        }
+    }
+}
---
+++ LightweightHTTPAPICaller/HTTPApiCaller.cs
@@ -0,0 +1,70 @@
+﻿
+namespace LightweightHTTPAPICaller
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+
+    public class HTTPAPICaller {
+        private HTTPAPIConfig config;
+        public HTTPAPICaller(HTTPAPIConfig config) {
+            this.config = config;
+        }
+
+        public async Task<ReturnType> GET<ReturnType>(string path, QueryParameters parameters) {
+            using (var client = new HttpClient()) {
+                setUpClient(client, parameters);
+
+                HttpResponseMessage response = await client.GetAsync(buildPath(path) + parameters);
+
+                return await handleResponse<ReturnType>(response, parameters);
+            }
+        }
+
+        public async Task<ReturnType> POST<ReturnType>(string path, QueryParameters parameters) {
+            using (var client = new HttpClient()) {
+                setUpClient(client, parameters);
+
+                FormUrlEncodedContent urlContent = new FormUrlEncodedContent(parameters.parameters);
+                HttpResponseMessage response = await client.PostAsync(buildPath(path), urlContent);
+
+                return await handleResponse<ReturnType>(response, parameters);
+            }
+        }
+
+        public async Task<ReturnType> PUT<ReturnType>(string path, QueryParameters parameters) {
+            using (var client = new HttpClient()) {
+                setUpClient(client, parameters);
+
+                FormUrlEncodedContent urlContent = new FormUrlEncodedContent(parameters.parameters);
+                HttpResponseMessage response = await client.PutAsync(buildPath(path), urlContent);
+
+                return await handleResponse<ReturnType>(response, parameters);
+            }
+        }
+
+        private string buildPath(string path) {
+            return config.httpAddress + path + "." + config.getResponseType();
+        }
+
+        private void setUpClient(HttpClient client, QueryParameters parameters) {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            parameters.Add("api_key", config.apiKey);
+        }
+
+        private async Task<ReturnType> handleResponse<ReturnType>(HttpResponseMessage response, QueryParameters parameters) {
+            if (response.IsSuccessStatusCode) {
+                return await response.Content.ReadAsAsync<ReturnType>();
+            } else {
+                Console.WriteLine(response);
+                Console.WriteLine(parameters);
+                throw new CouldNotReceiveResponse();
+            }
+        }
+    }
+}
